933609546637a6e1207f3ee4b9f74e7f605c31ed
Laure <laure.milian@gmail.com>
2016-09-29 15:24:12 +0200

    Initial commit

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
 console.log('have fun !');
+
+$("button").click(function(){
+	var personne = {};
+	personne.username = $("#first_name").val();
+	personne.last_name = $("#last_name").val();
+	personne.city = $("#city").val(); 
+
+	console.log(personne);
+
+	$("#username").html(personne.username + " " + personne.last_name);
+
+});
